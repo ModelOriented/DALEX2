0a7f653526873c4265562379884ca996b7ff6596
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-11-26 12:31:18 +0100

    shall we remove all links to ggplot2?

--- R/theme_mi2.R
+++ R/theme_mi2.R
@@ -7,17 +7,19 @@
 theme_mi2 <- function() {
   # sans instead of Tahoma to avoid problems with fonts
   # please fix some day
-  theme(axis.ticks = element_line(linetype = "blank"),
-        axis.title = element_text(family = "sans"),
-        plot.title = element_text(family = "sans"),
-        legend.text = element_text(family = "sans"),
-        legend.title = element_text(family = "sans"),
-        panel.background = element_rect(fill = "#f5f5f5"),
-        plot.background = element_rect(fill = "#f5f5f5",
-                                       colour = "aliceblue", size = 0.8,
-                                       linetype = "dotted"), strip.background = element_rect(fill = "gray50"),
-        strip.text = element_text(family = "sans"),
-        legend.key = element_rect(fill = NA, colour = NA,
-                                  size = 0),
-        legend.background = element_rect(fill = NA))
+  #
+  # Should we remove all liks to ggplot2
+  # theme(axis.ticks = element_line(linetype = "blank"),
+  #       axis.title = element_text(family = "sans"),
+  #       plot.title = element_text(family = "sans"),
+  #       legend.text = element_text(family = "sans"),
+  #       legend.title = element_text(family = "sans"),
+  #       panel.background = element_rect(fill = "#f5f5f5"),
+  #       plot.background = element_rect(fill = "#f5f5f5",
+  #                                      colour = "aliceblue", size = 0.8,
+  #                                      linetype = "dotted"), strip.background = element_rect(fill = "gray50"),
+  #       strip.text = element_text(family = "sans"),
+  #       legend.key = element_rect(fill = NA, colour = NA,
+  #                                 size = 0),
+  #       legend.background = element_rect(fill = NA))
 }
