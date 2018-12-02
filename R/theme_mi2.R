9d1f92636aa0fd5d9e2b96da0a068f25b42538dc
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-12-02 16:30:37 +0100

    updated tests and examples

--- R/theme_mi2.R
+++ R/theme_mi2.R
@@ -1,25 +1,170 @@
 #' MI^2 Theme
 #'
+#' Themem oject. In order to avoid an additional dependency from ggplot2, we have here a raw vaerion of an theme object
+#'
 #' @return theme object that can be added to ggplot2 plots
 #'
 #' @export
 #'
 theme_mi2 <- function() {
-  # sans instead of Tahoma to avoid problems with fonts
-  # please fix some day
-  #
-  # Should we remove all liks to ggplot2
-  # theme(axis.ticks = element_line(linetype = "blank"),
-  #       axis.title = element_text(family = "sans"),
-  #       plot.title = element_text(family = "sans"),
-  #       legend.text = element_text(family = "sans"),
-  #       legend.title = element_text(family = "sans"),
-  #       panel.background = element_rect(fill = "#f5f5f5"),
-  #       plot.background = element_rect(fill = "#f5f5f5",
-  #                                      colour = "aliceblue", size = 0.8,
-  #                                      linetype = "dotted"), strip.background = element_rect(fill = "gray50"),
-  #       strip.text = element_text(family = "sans"),
-  #       legend.key = element_rect(fill = NA, colour = NA,
-  #                                 size = 0),
-  #       legend.background = element_rect(fill = NA))
-}
+  structure(
+    list(
+      structure(
+        list("sans", NULL, NULL, NULL, NULL,
+             NULL, NULL, NULL, NULL, NULL, FALSE),
+        .Names = c(
+          "family",
+          "face",
+          "colour",
+          "size",
+          "hjust",
+          "vjust",
+          "angle",
+          "lineheight",
+          "margin",
+          "debug",
+          "inherit.blank"
+        ),
+        class = c("element_text",
+                  "element")
+      ),
+      structure(
+        list(NULL, NULL, "blank", NULL, FALSE,
+             FALSE),
+        .Names = c(
+          "colour",
+          "size",
+          "linetype",
+          "lineend",
+          "arrow",
+          "inherit.blank"
+        ),
+        class = c("element_line", "element")
+      ),
+      structure(
+        list(NA, NULL, NULL, NULL, FALSE),
+        .Names = c("fill",
+                   "colour", "size", "linetype", "inherit.blank"),
+        class = c("element_rect",
+                  "element")
+      ),
+      structure(
+        list(NA, NA, 0, NULL, FALSE),
+        .Names = c("fill",
+                   "colour", "size", "linetype", "inherit.blank"),
+        class = c("element_rect",
+                  "element")
+      ),
+      structure(
+        list("sans", NULL, NULL, NULL, NULL, NULL,
+             NULL, NULL, NULL, NULL, FALSE),
+        .Names = c(
+          "family",
+          "face",
+          "colour",
+          "size",
+          "hjust",
+          "vjust",
+          "angle",
+          "lineheight",
+          "margin",
+          "debug",
+          "inherit.blank"
+        ),
+        class = c("element_text", "element")
+      ),
+      structure(
+        list("sans", NULL, NULL, NULL, NULL, NULL, NULL,
+             NULL, NULL, NULL, FALSE),
+        .Names = c(
+          "family",
+          "face",
+          "colour",
+          "size",
+          "hjust",
+          "vjust",
+          "angle",
+          "lineheight",
+          "margin",
+          "debug",
+          "inherit.blank"
+        ),
+        class = c("element_text", "element")
+      ),
+      structure(
+        list("#f5f5f5", NULL, NULL, NULL, FALSE),
+        .Names = c("fill", "colour",
+                   "size", "linetype", "inherit.blank"),
+        class = c("element_rect",
+                  "element")
+      ),
+      structure(
+        list("#f5f5f5", "aliceblue", 0.8, "dotted",
+             FALSE),
+        .Names = c("fill", "colour", "size", "linetype",
+                   "inherit.blank"),
+        class = c("element_rect", "element")
+      ),
+      structure(
+        list("sans", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+             FALSE),
+        .Names = c(
+          "family",
+          "face",
+          "colour",
+          "size",
+          "hjust",
+          "vjust",
+          "angle",
+          "lineheight",
+          "margin",
+          "debug",
+          "inherit.blank"
+        ),
+        class = c("element_text", "element")
+      ),
+      structure(
+        list("gray50",
+             NULL, NULL, NULL, FALSE),
+        .Names = c("fill", "colour", "size",
+                   "linetype", "inherit.blank"),
+        class = c("element_rect", "element")
+      ),
+      structure(
+        list("sans", NULL, NULL, NULL, NULL, NULL, NULL,
+             NULL, NULL, NULL, FALSE),
+        .Names = c(
+          "family",
+          "face",
+          "colour",
+          "size",
+          "hjust",
+          "vjust",
+          "angle",
+          "lineheight",
+          "margin",
+          "debug",
+          "inherit.blank"
+        ),
+        class = c("element_text", "element")
+      )
+    ),
+    .Names = c(
+      "axis.title",
+      "axis.ticks",
+      "legend.background",
+      "legend.key",
+      "legend.text",
+      "legend.title",
+      "panel.background",
+      "plot.background",
+      "plot.title",
+      "strip.background",
+      "strip.text"
+    ),
+    class = c("theme", "gg"),
+    complete = FALSE,
+    validate = TRUE
+  )
+
+  }
--- tests/testthat/test_explain.R
+++ tests/testthat/test_explain.R
@@ -1,10 +1,11 @@
 context("Check explain() function")
 
 test_that("Type of data in the explainer",{
-  apartmentsTest_tibble <- dplyr::as_tibble(apartmentsTest)
+  library("dplyr")
+  apartments_test_tibble <- dplyr::as_tibble(apartments_test)
   model_liniowy <- lm(m2.price ~ construction.year + surface + floor + no.rooms + district, data = apartments)
 
-  explainer_lm <- DALEX::explain(model_liniowy, data = apartmentsTest_tibble[,2:6], y = apartmentsTest_tibble$m2.price)
+  explainer_lm <- explain(model_liniowy, data = apartments_test_tibble[,2:6], y = apartments_test_tibble$m2.price)
 
   expect_true(is.data.frame(explainer_lm$data))
 })
