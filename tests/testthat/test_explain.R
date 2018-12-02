c08c8137bb1aa1033a46f0ab2a69df7246741bce
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2018-12-02 18:16:43 +0100

    tibble is used in tests

--- tests/testthat/test_explain.R
+++ tests/testthat/test_explain.R
@@ -1,8 +1,8 @@
 context("Check explain() function")
 
 test_that("Type of data in the explainer",{
-  library("dplyr", warn.conflicts = FALSE, quietly = TRUE)
-  apartments_test_tibble <- dplyr::as_tibble(apartments_test)
+  library("tibble", warn.conflicts = FALSE, quietly = TRUE)
+  apartments_test_tibble <- tibble::as_tibble(apartments_test)
   model_liniowy <- lm(m2.price ~ construction.year + surface + floor + no.rooms + district, data = apartments)
 
   explainer_lm <- explain(model_liniowy, data = apartments_test_tibble[,2:6], y = apartments_test_tibble$m2.price)
